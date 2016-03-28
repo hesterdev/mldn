56a2603c0e49dc0ee29b0c7e6aa7d204a129a9d0
hesterdev <940356013@qq.com>
2016-03-28 13:28:00 +0800

    两次修改，却只有第一次提交到缓存区

--- hello.java
+++ hello.java
@@ -2,5 +2,7 @@ public class Hello{
 	public static void main(String argv[]){
         System.out.println("Hello Yootk.");
         System.out.println("Yootk.com");
+        
+        // 第一次修改
 	}
 }                   
